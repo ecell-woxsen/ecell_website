8d44b05f88b20c5fa56b55920487f8019c467ed7
Imad-81 <shaikimaduddin10@gmail.com>
2026-06-16 23:12:44 +0530

    edited logo

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -3,8 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL || "https://happy-animal-123.convex.cloud";
-const convex = new ConvexReactClient(convexUrl);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
   children,
